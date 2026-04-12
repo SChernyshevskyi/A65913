b41425cbb68998cf949a1441fd94290abf43c3bb
SChernyshevskyi <evg211877@gmail.com>
2026-04-12 18:53:01 +0200

    update UI Fix2

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://a65913-frontend-430164034378.europe-west1.run.app",
+  baseURL: "https://a65913-backend-430164034378.europe-west1.run.app",
 });
 
 export default api;
